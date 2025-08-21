67f244d4e4ac0d23e09da39ea6f4f942eac97dd3
Justine <justinemationg12@gmail.com>
2025-08-21 11:20:07 +0800

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kf4j9fii6v8"
+    "revision": "0.6fkdtaga088"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
